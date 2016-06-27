e1932eb7c74f729a78dbdace07454107e3bd4dc4
sapnasaxena <sapna.saxena@porteamedical.com>
2016-06-27 14:56:55 +0530

    Add files via upload

--- ClinicianAppTest/src/test/java/com/qa/mobileapp/common/TestBase.java
+++ ClinicianAppTest/src/test/java/com/qa/mobileapp/common/TestBase.java
@@ -1,47 +1,35 @@
 package com.qa.mobileapp.common;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.AndroidMobileCapabilityType;
+//import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.ObjectInputStream.GetField;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-import javax.imageio.stream.FileImageInputStream;
-
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
-
 import com.qa.mobileapp.pages.AlertPage;
 import com.qa.mobileapp.pages.AppointmentPage;
 import com.qa.mobileapp.pages.CalendarPage;
 import com.qa.mobileapp.pages.CampPage;
 import com.qa.mobileapp.pages.CampPageS;
 import com.qa.mobileapp.pages.CaseFilePage;
+import com.qa.mobileapp.pages.CheckInCheckOutPage;
 import com.qa.mobileapp.pages.DashboardPage;
 import com.qa.mobileapp.pages.FeedbackPage;
 import com.qa.mobileapp.pages.ForgotPasswordPage;
-import com.qa.mobileapp.pages.LeavePage;
+import com.qa.mobileapp.pages.LabAutomationPage;
 import com.qa.mobileapp.pages.LeavePage;
 import com.qa.mobileapp.pages.LogOutPage;
 import com.qa.mobileapp.pages.LoginPage;
 import com.qa.mobileapp.pages.PatientPage;
 import com.qa.mobileapp.pages.PaymentPage;
 import com.qa.mobileapp.pages.ProfilePage;
-import com.qa.mobileapp.pages.ReferPatientPage;
 import com.qa.mobileapp.pages.ReferPatientPage;
 import com.qa.mobileapp.pages.ReportsPage;
 
@@ -73,21 +61,23 @@ public class TestBase {
 	protected AlertPage alertpages;
 	protected CalendarPage calendarpages;
 	protected ForgotPasswordPage forgotpasswordpage;
+	protected CheckInCheckOutPage checkincheckoutpage;
+	protected LabAutomationPage labautomationpage;
 
 	@BeforeClass(alwaysRun=true)
 	public void setUp() throws Exception {		
-		File app = new File(System.getProperty("user.dir")+"/src/test/resources/android","CA_V33.1_STAGE_260416.apk");
+		File app = new File(System.getProperty("user.dir")+"/src/test/resources/android","CA_v34.0_48_STAGE_24062016_03.apk");
 		capabilities = new DesiredCapabilities(); 
 		capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
 		capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "21.0");
 		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "GenyAVD");
 		capabilities.setCapability(MobileCapabilityType.APP,app.getAbsolutePath());
-		capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,"com.portea.internal.app.activity.SplashActivity");
-		capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE,"com.healthvista.clinicianapp.stage");
-		capabilities.setCapability(AndroidMobileCapabilityType.APP_WAIT_ACTIVITY,"com.portea.internal.app.activity.SplashActivity");
+		capabilities.setCapability(MobileCapabilityType.APP_ACTIVITY,"com.portea.internal.app.activity.SplashActivity");
+		capabilities.setCapability(MobileCapabilityType.APP_PACKAGE,"com.healthvista.clinicianapp.stage");
+		capabilities.setCapability(MobileCapabilityType.APP_WAIT_ACTIVITY,"com.portea.internal.app.activity.SplashActivity");
 		capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"Appium");
 		capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 600);
-		driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		driver = new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 	}
 
@@ -96,6 +86,8 @@ public class TestBase {
 		GlobalUtil.wait(2);
 		loginpage = new LoginPage(driver);
 		loginpage.login(userName, password);
+		dashboardpage = new DashboardPage(driver);
+	
 	}
 	
 	@AfterClass(alwaysRun=true)
