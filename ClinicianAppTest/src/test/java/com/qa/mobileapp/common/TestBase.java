60c8346ce28504cc34b28cbf2b51fed6bb2c0692
sapnasaxena <sapna.saxena@porteamedical.com>
2016-09-16 14:04:02 +0530

    updated latest apk

--- ClinicianAppTest/src/test/java/com/qa/mobileapp/common/TestBase.java
+++ ClinicianAppTest/src/test/java/com/qa/mobileapp/common/TestBase.java
@@ -71,7 +71,7 @@ public class TestBase {
 
 	@BeforeClass(alwaysRun=true)
 	public void setUp() throws Exception {		
-		File app = new File(System.getProperty("user.dir")+"/src/test/resources/android","clinicianApp-staging-release_V_34.1.1(51)_08092016.apk");
+		File app = new File(System.getProperty("user.dir")+"/src/test/resources/android","CA_v34.1.1_STAGE_15092016_02.apk");
 		capabilities = new DesiredCapabilities(); 
 		capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
 		capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "23.0");
