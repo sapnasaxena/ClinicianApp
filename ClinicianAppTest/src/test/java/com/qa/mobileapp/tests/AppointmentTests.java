73944fed9068a91bc15fefb782b221b1135a677c
sapnasaxena <sapna.saxena@porteamedical.com>
2017-04-28 15:39:02 +0530

    Update AppointmentTests.java

--- ClinicianAppTest/src/test/java/com/qa/mobileapp/tests/AppointmentTests.java
+++ ClinicianAppTest/src/test/java/com/qa/mobileapp/tests/AppointmentTests.java
@@ -40,12 +40,7 @@ public class AppointmentTests extends TestBase {
 	
 	@BeforeMethod(alwaysRun=true)
 	public void initPageObjects(){
-		driver.findElement(By.name("Allow")).click();
-		GlobalUtil.wait(2);
-		driver.findElement(By.name("Allow")).click();
-		GlobalUtil.wait(2);
-		driver.findElement(By.name("Allow")).click();
-		GlobalUtil.wait(2);
+		
 		dashboardpage = new DashboardPage(driver);
 		appointmentpage = new AppointmentPage(driver);
 	}
